36a61361a3517b6db7e1b9d55cf07931da3787be
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-06-19 11:05:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,19 @@ for (var i=1;i<=20;i=i+1)
 		}
 }
 
-//作业2：
+//作业2：写程序输出9*9乘法表
+var row = 9;			
+var colum = 9;			
+document.write("<table border=0>");			
+for(i = 1; i <= row; i++){			//循环控制行				
+	document.write("<tr>");				
+	for (j = 1; j <= colum; j++){		//循环控制列					
+		if (j <= i){				
+			document.write("<td>"+ i + "*" + j  + "=" + i*j + "</td>");					
+		}else{						
+			document.write("<td> </td>");					
+		}			
+	}				
+	document.write("</tr>");		
+}			
+document.write("</table>");
